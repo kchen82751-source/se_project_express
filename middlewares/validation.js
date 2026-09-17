5cf1d13fe66684dea28816829c93ea4ee90a4638
PracticumTutor <PracticumTutor@gmail.com>
2026-09-17 11:25:18 -0400

    New update for routes/clothingitems.js

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -27,15 +27,6 @@ module.exports.validateId = celebrate({
   params: Joi.object().keys({
     itemId: Joi.string().hex().length(24),
   }),
-  name: Joi.string().required().min(2).max(30).messages({
-    "string.min": 'The minimum length of the "name" field is 2',
-    "string.max": 'The maximum length of the "name" field is 30',
-    "string.empty": 'The "name" field must be filled in',
-  }),
-  imageUrl: Joi.string().required().custom(validateURL).messages({
-    "string.empty": 'The "imageUrl" field must be filled in',
-    "string.uri": 'the "imageUrl" field must be a valid url',
-  }),
 });
 
 module.exports.validateSignIn = celebrate({
@@ -108,45 +99,3 @@ module.exports.validateCreateItem = celebrate({
     weather: Joi.string().valid("hot", "warm", "cold").required(),
   }),
 });
-
-// module.exports.validateDeleteItem = celebrate({
-//   body: Joi.object().keys({
-//     name: Joi.string().required().min(2).max(30).messages({
-//       "string.min": 'The minimum length of the "name" field is 2',
-//       "string.max": 'The maximum length of the "name" field is 30',
-//       "string.empty": 'The "name" field must be filled in',
-//     }),
-//     imageUrl: Joi.string().required().custom(validateURL).messages({
-//       "string.empty": 'The "imageUrl" field must be filled in',
-//       "string.uri": 'the "imageUrl" field must be a valid url',
-//     }),
-//   }),
-// });
-
-// module.exports.validateLikesItem = celebrate({
-//   body: Joi.object().keys({
-//     name: Joi.string().required().min(2).max(30).messages({
-//       "string.min": 'The minimum length of the "name" field is 2',
-//       "string.max": 'The maximum length of the "name" field is 30',
-//       "string.empty": 'The "name" field must be filled in',
-//     }),
-//     imageUrl: Joi.string().required().custom(validateURL).messages({
-//       "string.empty": 'The "imageUrl" field must be filled in',
-//       "string.uri": 'the "imageUrl" field must be a valid url',
-//     }),
-//   }),
-// });
-
-// module.exports.validateDislikesItem = celebrate({
-//   body: Joi.object().keys({
-//     name: Joi.string().required().min(2).max(30).messages({
-//       "string.min": 'The minimum length of the "name" field is 2',
-//       "string.max": 'The maximum length of the "name" field is 30',
-//       "string.empty": 'The "name" field must be filled in',
-//     }),
-//     imageUrl: Joi.string().required().custom(validateURL).messages({
-//       "string.empty": 'The "imageUrl" field must be filled in',
-//       "string.uri": 'the "imageUrl" field must be a valid url',
-//     }),
-//   }),
-// });
